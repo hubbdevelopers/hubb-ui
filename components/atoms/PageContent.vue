dc62f48c12f892eaf52560efdc794d689b061382
kimura.m <k422756+git@gmail.com>
2019-05-13 00:17:20 +0900

    ページのサニタイズ内容の変更

--- components/atoms/PageContent.vue
+++ components/atoms/PageContent.vue
@@ -1,5 +1,48 @@
 <template>
-  <div v-html="$sanitize(content)" class="content" />
+  <div
+    v-html="
+      $sanitize(content, {
+        allowedTags: [
+          'img',
+          'iframe',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+          'blockquote',
+          'p',
+          'a',
+          'ul',
+          'ol',
+          'nl',
+          'li',
+          'b',
+          'i',
+          'strong',
+          'em',
+          'strike',
+          'code',
+          'hr',
+          'br',
+          'div',
+          'table',
+          'thead',
+          'caption',
+          'tbody',
+          'tr',
+          'th',
+          'td',
+          'pre'
+        ],
+        allowedAttributes: {
+          img: ['src'],
+          iframe: ['src']
+        },
+        allowedIframeHostnames: ['www.youtube.com']
+      })
+    "
+    class="content"
+  />
 </template>
 <script>
 export default {
