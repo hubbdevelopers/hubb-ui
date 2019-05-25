ccfc6ecd0f35c40d1426507f08ef2d9322220af9
kimura.m <k422756+git@gmail.com>
2019-05-25 10:42:33 +0900

    storeのリファクタリング

--- store/user.ts
+++ store/user.ts
@@ -20,14 +20,14 @@ export interface UsersState {
   email: string
 }
 
-export const state: UsersState = {
+export const state = () => ({
   data: blankUser.data,
   pages: [],
   timeline: [],
   notifications: [],
   id: '', // Firebase UID
   email: ''
-}
+})
 
 export const actions: ActionTree<UsersState, RootState> = {
   initUser({ state, commit, dispatch }) {
@@ -48,424 +48,381 @@ export const actions: ActionTree<UsersState, RootState> = {
     })
   },
 
-  signup({ commit }, param) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const authData = await auth.createUserWithEmailAndPassword(
-          param.email,
-          param.password
-        )
-        await db
-          .collection('users')
-          .doc(authData.user!.uid)
-          .set({
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-            updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
+  async signup({ commit }, param) {
+    try {
+      const authData = await auth.createUserWithEmailAndPassword(
+        param.email,
+        param.password
+      )
+      await db
+        .collection('users')
+        .doc(authData.user!.uid)
+        .set({
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
 
-        commit('updateId', authData.user!.uid)
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+      commit('updateId', authData.user!.uid)
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  reauthenticate({ state }, param) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const user = auth.currentUser
-        const credentials = firebase.auth.EmailAuthProvider.credential(
-          state.email,
-          param.password
-        )
+  async reauthenticate({ state }, param) {
+    try {
+      const user = auth.currentUser
+      const credentials = firebase.auth.EmailAuthProvider.credential(
+        state.email,
+        param.password
+      )
 
-        await user!.reauthenticateAndRetrieveDataWithCredential(credentials)
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+      await user!.reauthenticateAndRetrieveDataWithCredential(credentials)
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  deleteUser({ commit }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const user = auth.currentUser
-        await user!.delete()
-        commit('clearUserState')
-        resolve()
-      } catch (e) {
-        console.log(e)
-        reject(e)
-      }
-    })
+  async deleteUser({ commit }) {
+    try {
+      const user = auth.currentUser
+      await user!.delete()
+      commit('clearUserState')
+      return
+    } catch (e) {
+      console.log(e)
+      return e
+    }
   },
 
   // eslint-disable-next-line no-empty-pattern
-  updatePassword({}, param) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const user = auth.currentUser
-        await user!.updatePassword(param.password)
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+  async updatePassword({}, param) {
+    try {
+      const user = auth.currentUser
+      await user!.updatePassword(param.password)
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  logout({ commit }) {
-    return new Promise((resolve, reject) => {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          commit('clearUserState')
-          resolve()
+  async logout({ commit }) {
+    try {
+      await firebase.auth().signOut()
+      commit('clearUserState')
+      return
+    } catch (e) {
+      return e
+    }
+  },
+
+  async initialUpdate({ state, dispatch }, { accountId, name }) {
+    try {
+      await db
+        .collection('users')
+        .doc(state.id)
+        .set({
+          accountId: accountId,
+          name: name,
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
         })
-        .catch(e => {
-          reject(e)
-        })
-    })
+      dispatch('fetchUser')
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  initialUpdate({ state, dispatch }, { accountId, name }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('users')
-          .doc(state.id)
-          .set({
-            accountId: accountId,
-            name: name,
-            updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
-        dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
-  },
-
-  updateProfile(
+  async updateProfile(
     { dispatch, state },
     { name, description, birthday, homepage, twitter, facebook, instagram }
   ) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const param = {
+    try {
+      const param = {
+        name: name,
+        description: description,
+        birthday: birthday,
+        homepage: homepage,
+        twitter: twitter,
+        facebook: facebook,
+        instagram: instagram,
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+      }
+      await db
+        .collection('users')
+        .doc(state.id)
+        .update(param)
+
+      dispatch('fetchUser')
+      return
+    } catch (e) {
+      console.error('Error writing document: ', e)
+      return e
+    }
+  },
+
+  async updateImage({ state, commit }, imageBlob) {
+    const imagePath = 'users/' + state.id + '/main-image'
+    const imageRef = storageRef.child(imagePath)
+
+    try {
+      await imageRef.put(imageBlob)
+      const url = await imageRef.getDownloadURL()
+
+      await db
+        .collection('users')
+        .doc(state.id)
+        .update({
+          image: url,
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+      commit('updateImage', url)
+      return
+    } catch (e) {
+      console.error('Error writing document: ', e)
+      return e
+    }
+  },
+
+  async createPage({ state, dispatch }, pageName = 'untitled') {
+    try {
+      const param = {
+        name: pageName,
+        ownerId: state.id,
+        ownerType: 'user',
+        isDraft: true,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp()
+      }
+      const doc = await db.collection('pages').add(param)
+      dispatch('fetchPages')
+      return doc.id
+    } catch (e) {
+      console.error('Error writing document: ', e)
+      return e
+    }
+  },
+
+  async updatePage({ dispatch }, { id, isDraft, name, content, image }) {
+    try {
+      await db
+        .collection('pages')
+        .doc(id)
+        .update({
+          isDraft: isDraft,
           name: name,
-          description: description,
-          birthday: birthday,
-          homepage: homepage,
-          twitter: twitter,
-          facebook: facebook,
-          instagram: instagram,
+          content: content,
+          image: image,
           updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-        }
-        await db
-          .collection('users')
-          .doc(state.id)
-          .update(param)
-
-        dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        console.error('Error writing document: ', e)
-        reject(e)
-      }
-    })
+        })
+      dispatch('fetchPages')
+      return
+    } catch (e) {
+      console.error('Error writing document: ', e)
+      return e
+    }
   },
 
-  updateImage({ state, commit }, imageBlob) {
-    return new Promise(async (resolve, reject) => {
-      const imagePath = 'users/' + state.id + '/main-image'
-      const imageRef = storageRef.child(imagePath)
-
-      try {
-        await imageRef.put(imageBlob)
-        const url = await imageRef.getDownloadURL()
-
-        await db
-          .collection('users')
-          .doc(state.id)
-          .update({
-            image: url,
-            updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
-        commit('updateImage', url)
-        resolve()
-      } catch (e) {
-        console.error('Error writing document: ', e)
-        reject(e)
-      }
-    })
-  },
-
-  createPage({ state, dispatch }, pageName = 'untitled') {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const param = {
-          name: pageName,
-          ownerId: state.id,
-          ownerType: 'user',
-          isDraft: true,
-          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-          updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-        }
-        const doc = await db.collection('pages').add(param)
-        dispatch('fetchPages')
-        resolve(doc.id)
-      } catch (e) {
-        console.error('Error writing document: ', e)
-        reject(e)
-      }
-    })
-  },
-
-  updatePage({ dispatch }, { id, isDraft, name, content, image }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('pages')
-          .doc(id)
-          .update({
-            isDraft: isDraft,
-            name: name,
-            content: content,
-            image: image,
-            updatedAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
-        dispatch('fetchPages')
-        resolve()
-      } catch (e) {
-        console.error('Error writing document: ', e)
-        reject()
-      }
-    })
-  },
-
-  deletePage({ dispatch }, pageId) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('pages')
-          .doc(pageId)
-          .delete()
-        //console.log(ret)
-        dispatch('fetchPages')
-        resolve()
-      } catch (e) {
-        console.log(e)
-        reject(e)
-      }
-    })
+  async deletePage({ dispatch }, pageId) {
+    try {
+      await db
+        .collection('pages')
+        .doc(pageId)
+        .delete()
+      //console.log(ret)
+      dispatch('fetchPages')
+      return
+    } catch (e) {
+      console.log(e)
+      return e
+    }
   },
 
   async fetchUser({ commit, state }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const doc = await db
-          .collection('users')
-          .doc(state.id)
-          .get()
+    try {
+      const doc = await db
+        .collection('users')
+        .doc(state.id)
+        .get()
 
-        if (doc.exists) {
-          const user = doc.data()
-          commit('updateUser', user)
-        } else {
-          console.log('No such document!')
-        }
-        resolve()
-      } catch (e) {
-        reject()
+      if (doc.exists) {
+        const user = doc.data()
+        commit('updateUser', user)
+      } else {
+        console.log('No such document!')
       }
-    })
+      return
+    } catch (e) {
+      return e
+    }
   },
 
   async fetchPages({ commit, state }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const query = await db
-          .collection('pages')
-          .where('ownerType', '==', 'user')
-          .where('ownerId', '==', state.id)
-          .get()
+    try {
+      const query = await db
+        .collection('pages')
+        .where('ownerType', '==', 'user')
+        .where('ownerId', '==', state.id)
+        .get()
 
-        if (query.size > 0) {
-          const pages = query.docs.map(
-            (doc): Page => {
-              return { id: doc.id, likedBy: [], data: doc.data() as PageData }
-            }
-          )
-          commit('updatePages', pages)
-        }
-
-        resolve()
-      } catch (e) {
-        console.log(e)
-        reject(e)
-      }
-    })
-  },
-
-  async fetchTimeline({ commit, state }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const snapshot = await db
-          .collection('users')
-          .doc(state.id)
-          .collection('timeline')
-          .orderBy('createdAt', 'desc')
-          .limit(50)
-          .get()
-
-        const timeline: Page[] = snapshot.docs.map(
+      if (query.size > 0) {
+        const pages = query.docs.map(
           (doc): Page => {
             return { id: doc.id, likedBy: [], data: doc.data() as PageData }
           }
         )
-
-        commit('updateTimeline', timeline)
-        resolve()
-      } catch (e) {
-        console.log(e)
-        reject(e)
+        commit('updatePages', pages)
       }
-    })
+
+      return
+    } catch (e) {
+      console.log(e)
+      return e
+    }
   },
 
-  followUser({ commit, state }, followingId) {
+  async fetchTimeline({ commit, state }) {
+    try {
+      const snapshot = await db
+        .collection('users')
+        .doc(state.id)
+        .collection('timeline')
+        .orderBy('createdAt', 'desc')
+        .limit(50)
+        .get()
+
+      const timeline: Page[] = snapshot.docs.map(
+        (doc): Page => {
+          return { id: doc.id, likedBy: [], data: doc.data() as PageData }
+        }
+      )
+
+      commit('updateTimeline', timeline)
+      return
+    } catch (e) {
+      console.log(e)
+      return e
+    }
+  },
+
+  async followUser({ commit, state }, followingId) {
     if (state.id === followingId) {
       return
     }
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('users')
-          .doc(state.id)
-          .collection('follows')
-          .doc()
-          .set({
-            from: state.id,
-            to: followingId,
-            type: 'user',
-            createdAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
-
-        commit('addFollowing', followingId)
-        //dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
-  },
-
-  unfollowUser({ commit, state }, followingId) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const followingQuery = await db
-          .collection('users')
-          .doc(state.id)
-          .collection('follows')
-          .where('to', '==', followingId)
-          .get()
-
-        followingQuery.forEach(async doc => {
-          await doc.ref.delete()
+    try {
+      await db
+        .collection('users')
+        .doc(state.id)
+        .collection('follows')
+        .doc()
+        .set({
+          from: state.id,
+          to: followingId,
+          type: 'user',
+          createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
 
-        commit('deleteFollowing', followingId)
-        //dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+      commit('addFollowing', followingId)
+      //dispatch('fetchUser')
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  likePage({ state, dispatch }, pageId) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('users')
-          .doc(state.id)
-          .collection('likes')
-          .doc()
-          .set({
-            from: state.id,
-            pageId: pageId,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
+  async unfollowUser({ commit, state }, followingId) {
+    try {
+      const followingQuery = await db
+        .collection('users')
+        .doc(state.id)
+        .collection('follows')
+        .where('to', '==', followingId)
+        .get()
 
-        dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        console.log(e)
-        reject(e)
-      }
-    })
+      followingQuery.forEach(async doc => {
+        await doc.ref.delete()
+      })
+
+      commit('deleteFollowing', followingId)
+      //dispatch('fetchUser')
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  unlikePage({ state, dispatch }, pageId) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const pageQuery = await db
-          .collection('users')
-          .doc(state.id)
-          .collection('likes')
-          .where('pageId', '==', pageId)
-          .get()
-
-        pageQuery.forEach(async doc => {
-          await doc.ref.delete()
+  async likePage({ state, dispatch }, pageId) {
+    try {
+      await db
+        .collection('users')
+        .doc(state.id)
+        .collection('likes')
+        .doc()
+        .set({
+          from: state.id,
+          pageId: pageId,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
 
-        dispatch('fetchUser')
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+      dispatch('fetchUser')
+      return
+    } catch (e) {
+      console.log(e)
+      return e
+    }
   },
 
-  createComment({ state }, { ownereId, pageId, text }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('pages')
-          .doc(pageId)
-          .collection('comments')
-          .add({
-            userId: state.id,
-            text: text,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp()
-          })
+  async unlikePage({ state, dispatch }, pageId) {
+    try {
+      const pageQuery = await db
+        .collection('users')
+        .doc(state.id)
+        .collection('likes')
+        .where('pageId', '==', pageId)
+        .get()
 
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+      pageQuery.forEach(async doc => {
+        await doc.ref.delete()
+      })
+
+      dispatch('fetchUser')
+      return
+    } catch (e) {
+      return e
+    }
   },
 
-  deleteComment({ state }, { pageId, commentId }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await db
-          .collection('pages')
-          .doc(pageId)
-          .collection('comments')
-          .doc(commentId)
-          .delete()
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
-    })
+  async createComment({ state }, { ownereId, pageId, text }) {
+    try {
+      await db
+        .collection('pages')
+        .doc(pageId)
+        .collection('comments')
+        .add({
+          userId: state.id,
+          text: text,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+      return
+    } catch (e) {
+      return e
+    }
+  },
+
+  async deleteComment({ state }, { pageId, commentId }) {
+    try {
+      await db
+        .collection('pages')
+        .doc(pageId)
+        .collection('comments')
+        .doc(commentId)
+        .delete()
+      return
+    } catch (e) {
+      return e
+    }
   }
 }
 
